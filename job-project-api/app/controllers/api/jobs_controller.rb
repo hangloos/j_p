21263d5a73597a2725cd279b7ca9d9be754487f9
Eric Loos <ericloos00@gmail.com>
2017-08-09 20:33:23 -0500

    added create, strong params, update and set job functions in jobs controller

--- job-project-api/app/controllers/api/jobs_controller.rb
+++ job-project-api/app/controllers/api/jobs_controller.rb
@@ -4,8 +4,38 @@ class Api::JobsController < ApplicationController
     render json: Job.all
   end
 
-  def show
-    render json: Job.find_by(id: params[:id])
+  def create
+    job = Job.new(job_params)
+    if job.save
+      render json: job
+    else
+      render json: { message: job.errors }, status: 422
+    end
   end
+
+  def show
+    render json: @job
+  end
+
+  def update
+    if @job.update(job_params)
+      render json: @job
+    else
+      render json: { message: @job.errors }, status: 422
+    end
+  end
+
+
+
+  private
+
+  def set_job
+    @job = Job.find_by(id: params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:id, :title, :description, :url, :company, :category_id, :type_id)
+  end
+
 
 end
