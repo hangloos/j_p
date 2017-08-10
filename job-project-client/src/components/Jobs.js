b3e4bc9c67281018a2b8f171223bd2897f377743
Eric Loos <ericloos00@gmail.com>
2017-08-09 21:35:16 -0500

    jobs component with fake information working

---
+++ job-project-client/src/components/Jobs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Jobs = (props) => (
+
+  <div>
+    <h1>Jobs</h1>
+      {props.jobs.map(job => 
+        <div>
+          <h3>{job.title}</h3>
+          <h2>{job.description}</h2>
+        </div>
+        )}
+        </div>
+      );
+
+export default Jobs;
--- job-project-client/src/containers/App.js
+++ job-project-client/src/containers/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Jobs from '../components/Jobs'
+
+const jobs = [
+  {
+    title: 'Accountant',
+    description: "hey this is an accountant role"
+  },
+  {
+    title: 'Developer',
+    description: "develop in this role"
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div>
-       App
+      <div className="App">
+       <Jobs jobs={jobs} />
       </div>
     );
   }
