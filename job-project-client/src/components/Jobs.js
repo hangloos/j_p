22f17eaeaec19be856c604ea7e74e495082e3fb4
Eric Loos <ericloos00@gmail.com>
2017-08-16 18:51:17 -0500

    added header, and some styling for the jobs listing page using bootstrap

--- job-project-client/src/components/Jobs.js
+++ job-project-client/src/components/Jobs.js
@@ -3,11 +3,24 @@ import React from 'react';
 const Jobs = (props) => (
 
   <div>
-    <h1>Jobs</h1>
-      {props.jobs.map(job => 
-        <div>
-          <h3>{job.title}</h3>
-          <h2>{job.description}</h2>
+  {props.jobs.map(job => 
+           <div className="col-xs-8 col-sm-4 col-md-4 col-lg-4">
+          <div className="thumbnail">
+              <div className="caption">
+                <div className='col-lg-4'>
+                    <span className="glyphicon glyphicon-credit-card"></span>
+                    <span className="glyphicon glyphicon-trash pull-right text-primary"></span>
+                </div>
+                <div className='col-lg-4 well well-add-card'>
+                    <h4>{job.title}</h4>
+                </div>
+                <div className='col-lg-4'>
+                    <p>4111xxxxxxxx3265</p>
+                    <p className="text-muted">Exp: 12-08</p>
+                </div>
+                <button type="button" className="btn btn-primary btn-xs btn-update btn-add-card">Update</button>
+            </div>
+          </div>
         </div>
         )}
         </div>
--- job-project-client/src/containers/App.js
+++ job-project-client/src/containers/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 import Jobs from '../components/Jobs'
+import Navbar from '../components/Navbar'
+import 'bootstrap/dist/css/bootstrap.css';
 
 const jobs = [
   {
     title: 'Accountant',
-    description: "hey this is an accountant role"
+    description: "hey this is an accountant role",
+    url: "www.google.com",
+    company: "Google"
   },
   {
     title: 'Developer',
-    description: "develop in this role"
+    description: "develop in this role",
+    url: "wwww.yahoo.com",
+    company: "Yahoo"
+  }, {
+    title: 'Developer',
+    description: "develop in this role",
+    url: "wwww.yahoo.com",
+    company: "Yahoo"
   }
 ]
+
+var navbar = {};
+navbar.brand = 
+  {linkTo: "#", text: "Eric Loos Project"};
+navbar.links = [
+  {linkTo: "#", text: "Home"},
+  // {linkTo: "#", text: "Link 2"},
+  // {dropdown: true, text: "Dropdown", links: [
+  //   {linkTo: "#", text: "Dropdown Link 1"},
+  //   {linkTo: "#", text: "Dropdown Link 2", active: true}
+  // ]}
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Jobs jobs={jobs} />
+      <Navbar {...navbar} />
+       <Jobs key={jobs.title} jobs={jobs} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+// https://bootsnipp.com/snippets/kEvrW
+
+// <Grid>
+//     <Row className="show-grid">
+//       <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+//       <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+//     </Row>
+// </Grid>
