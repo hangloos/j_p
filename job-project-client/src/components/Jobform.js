4ece97f853836e841aef73a5d41f1aaf22c32a21
Eric Loos <ericloos00@gmail.com>
2017-08-20 13:27:53 -0500

    add value forms to state on change. submit function on submit

--- job-project-client/src/components/Jobform.js
+++ job-project-client/src/components/Jobform.js
@@ -4,14 +4,24 @@ import './Jobs.css'
 
 export default class Jobform extends React.Component {
 
+  constructor(props) {
+    super(props)
 
-onChange() {
-  console.log("changing text")
+    this.state = {
+      
+    }
+  }
+
+
+onChange(e) {
+  const value = e.target.name
+  this.setState({
+    [e.target.name]: e.target.value
+  })
 }
 
 handleSubmit(e) {
 debugger
-e.preventDefault()
 }
 
   render() {
@@ -22,25 +32,25 @@ e.preventDefault()
                 <br />
               <h3 >Create Job</h3>
                     <div className="form-group">
-                    <input type="text" className="form-control" id="title" name="title" onChange={this.onChange} placeholder="Title" required />
+                    <input type="text" className="form-control" id="title" name="title" onChange={this.onChange.bind(this)} placeholder="Title" required />
                   </div>
                   <div className="form-group">
-                    <input type="text" className="form-control" id="company" name="company" placeholder="Company" required />
+                    <input type="text" className="form-control" id="company" name="company" onChange={this.onChange.bind(this)} placeholder="Company" required />
                   </div>
                   <div className="form-group">
-                  <textarea className="form-control" type="textarea" id="description" onChange={this.onChange}  placeholder="Job Description" rows="7"></textarea>
+                  <textarea className="form-control" type="textarea" id="description" name="description" onChange={this.onChange.bind(this)}  placeholder="Job Description" rows="7"></textarea>
                   </div>
                   <div className="form-group">
-                    <input type="url" className="form-control" id="url" name="url" onChange={this.onChange}  placeholder="Company Website" required />
+                    <input type="url" className="form-control" id="url" name="url" onChange={this.onChange.bind(this)}  placeholder="Company Website" required />
                   </div>
                   <div className="form-group">
-                  <select>
+                  <select onChange={this.onChange.bind(this)} >
                     <option value="Full-Time">Full-Time</option>
                     <option value="Part-Time">Part-Time</option>
                   </select>
                   </div>
-                <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Submit Form</button>
-              </form>
+                  <input type="submit" className="btn btn-success" value="Submit" />              
+                </form>
             </div>
 
         </div>
