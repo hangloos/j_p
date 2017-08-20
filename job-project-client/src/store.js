518d686a416f368f3b9d91bc81c57f7b26215d0a
Eric Loos <ericloos00@gmail.com>
2017-08-20 15:23:16 -0500

    create store file for redux

--- job-project-client/src/store.js
+++ job-project-client/src/store.js
@@ -4,3 +4,25 @@ import {
   combineReducers
 } from 'redux'
 import thunk from 'redux-thunk';
+
+const jobsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_JOBS_SUCCESS':
+      return action.jobs
+
+    default:
+      return state;
+  }
+}
+
+const reducers = combineReducers({
+  jobs: jobsReducer
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
